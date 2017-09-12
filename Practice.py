9ee6bbb0f8be6dd23c6fc1b2eaffe49560cb31e2
Er Xiao <voyager2015@126.com>
2017-09-12 20:56:55 +0800

    page45

--- Practice.py
+++ Practice.py
@@ -1,4 +1,20 @@
-age=20
-name='Swaroop'
-print('{} was {} years old when he wrote this book'.format(name,age))
-print('Why is {} playing with that python?'.format(name))
+# age=20
+# name='Swaroop'
+# print('{} was {} years old when he wrote this book'.format(name,age))
+# print('Why is {} playing with that python?'.format(name))
+# print('{0:.3f}'.format(1.0/3))
+# print('{0:_^11}'.format('hello'))
+# print('{name} wrote {book}'.format(name='Swaroop',book='A Byte of Python'))
+# print('a',end='')
+# print('b',end=' ')
+# print('c')
+# print("This is the first sentence. \
+# This is the second sentence.")
+# print(r"Newlines are indicated by \n")
+i=5
+print(i)
+i=i+1
+print(i)
+s='''This is a multi-line string.
+This is the second line.'''
+print(s)
